d8174181d0672131bccbc4ebee7fe67553be56ff
Florian Körner <647303+FlorianKoerner@users.noreply.github.com>
2024-05-27 21:06:14 +0200

    Remove: cli package

--- packages/@dicebear/cli/src/index.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * DiceBear (@dicebear/cli)
- *
- * Code licensed under MIT (https://github.com/dicebear/dicebear/blob/9.x/LICENSE)
- * Copyright (c) 2024 Florian Körner
- */
-
-export const foo = 'bar';
--- packages/@dicebear/cli/tests/index.test.js
+++
@@ -1,8 +0,0 @@
-import assert from 'node:assert';
-import { describe, it } from 'node:test';
-
-describe('index', () => {
-  it('foo', () => {
-    assert.ok(true);
-  });
-});
