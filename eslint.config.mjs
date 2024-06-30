42403171b97fe461bc23a52591b8aa08a9713963
Florian Körner <contact@florian-koerner.com>
2024-06-30 16:24:54 +0200

    Update eslint and prettier configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+
+export default tseslint.config(
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    ignores: ['apps/**', '**/lib/**/*.js'],
+  }
+);
