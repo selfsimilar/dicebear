0a9c11e9f35ff96cde3c4772e748623f6faca0a1
Florian Körner <647303+FlorianKoerner@users.noreply.github.com>
2024-08-10 17:37:38 +0200

    Add. Tailwind in docs

---
+++ apps/docs/tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'selector',
+  content: [
+    '.vitepress/playground/**/*.vue',
+    '.vitepress/shared/**/*.vue',
+    '.vitepress/theme/**/*.vue',
+  ],
+};
