92ec6cbf82f1e404df805a5cfcdaa73aac4cd36f
Florian Körner <contact@florian-koerner.com>
2024-08-17 12:03:39 +0200

    Add: GitHub Notification test

---
+++ apps/docs/.vitepress/theme/components/Notification.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <a
+    href="https://github.com/dicebear/dicebear"
+    class="notification"
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    ⭐️ DiceBear is fully open source! Please consider starring it on
+    <u>GitHub</u>. Thank you! ⭐️
+  </a>
+</template>
+
+<style lang="scss">
+.notification {
+  display: block;
+  padding: 6px 10px;
+  background-color: var(--vp-c-brand);
+  color: var(--vp-c-white);
+  text-decoration: none;
+  font-weight: 500;
+  text-align: center;
+  font-size: 14px;
+}
+</style>
